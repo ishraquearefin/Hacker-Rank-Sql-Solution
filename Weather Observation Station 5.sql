d03c99966b7ee62cb0becc5d7b7b52d81c4f554b
ishraquearefin <49182411+ishraquearefin@users.noreply.github.com>
2022-11-03 15:19:10 +0600

    Update Weather Observation Station 5.sql

--- Weather Observation Station 5.sql
+++ Weather Observation Station 5.sql
@@ -1,4 +1,5 @@
 /**Here we are selecting city and length of the city by ordering them according to their length and alphabetic order**/
+/**Order by function sort the data according to the condition and here are two conditions, one of them is length of city and another one is city alphabetic name order**/ 
 
 (Select CITY,LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) ASC,CITY ASC LIMIT 1) 
 UNION (Select CITY,LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) DESC,CITY ASC LIMIT 1)
